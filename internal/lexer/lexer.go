4582af3f1aecedd667f7608add3fb340acacb16f
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-05 04:33:21 +0500

    move input character method

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -11,6 +11,18 @@ func New(input string) *Lexer {
 	l := &Lexer{
 		input: []rune(input),
 	}
+	l.readChar()
 
 	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPos >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPos]
+	}
+
+	l.pos = l.readPos
+	l.readPos += 1
 }
