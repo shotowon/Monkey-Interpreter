432ece08ac927c63013c838c3914defa480124b2
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-05 07:25:23 +0500

    test extended keyword set

--- internal/lexer/lexer_test.go
+++ internal/lexer/lexer_test.go
@@ -94,7 +94,8 @@ func TestNextToken1(t *testing.T) {
 			let result = add(five, ten);
 			!-/*5;
 			5 < 10 > 5;
-			5 <= 5 >= 5;			`,
+			5 <= 5 >= 5;
+			`,
 			expected: []nextTokenExpectedValue{
 				{token.LET, "let"},
 				{token.ID, "five"},
@@ -157,6 +158,111 @@ func TestNextToken1(t *testing.T) {
 				{token.EQ, "="},
 				{token.INT, "5"},
 				{token.SEMICOLON, ";"},
+				{token.EOF, ""},
+			},
+		},
+		{
+			name: "Lexing extended keyword set",
+			input: `let five = 5;
+			let ten = 10;
+			let add = fn(x, y) {
+				x + y;
+			};
+			let result = add(five, ten);
+			!-/*5;
+			5 < 10 > 5;
+			5 <= 5 >= 5;
+			if (5 < 10) {
+				return true;
+			} else {
+				return false;
+			}`,
+			expected: []nextTokenExpectedValue{
+				{token.LET, "let"},
+				{token.ID, "five"},
+				{token.EQ, "="},
+				{token.INT, "5"},
+				{token.SEMICOLON, ";"},
+
+				{token.LET, "let"},
+				{token.ID, "ten"},
+				{token.EQ, "="},
+				{token.INT, "10"},
+				{token.SEMICOLON, ";"},
+
+				{token.LET, "let"},
+				{token.ID, "add"},
+				{token.EQ, "="},
+				{token.FUNC, "fn"},
+				{token.LPAREN, "("},
+				{token.ID, "x"},
+				{token.COMMA, ","},
+				{token.ID, "y"},
+				{token.RPAREN, ")"},
+				{token.LBRACE, "{"},
+				{token.ID, "x"},
+				{token.PLUS, "+"},
+				{token.ID, "y"},
+				{token.SEMICOLON, ";"},
+				{token.RBRACE, "}"},
+				{token.SEMICOLON, ";"},
+
+				{token.LET, "let"},
+				{token.ID, "result"},
+				{token.EQ, "="},
+				{token.ID, "add"},
+				{token.LPAREN, "("},
+				{token.ID, "five"},
+				{token.COMMA, ","},
+				{token.ID, "ten"},
+				{token.RPAREN, ")"},
+				{token.SEMICOLON, ";"},
+
+				{token.BANG, "!"},
+				{token.MINUS, "-"},
+				{token.SLASH, "/"},
+				{token.ASTERISK, "*"},
+				{token.INT, "5"},
+				{token.SEMICOLON, ";"},
+
+				{token.INT, "5"},
+				{token.LT, "<"},
+				{token.INT, "10"},
+				{token.GT, ">"},
+				{token.INT, "5"},
+				{token.SEMICOLON, ";"},
+				{token.INT, "5"},
+				{token.LT, "<"},
+				{token.EQ, "="},
+				{token.INT, "5"},
+				{token.GT, ">"},
+				{token.EQ, "="},
+				{token.INT, "5"},
+				{token.SEMICOLON, ";"},
+				/*
+					if (5 < 10) {
+						return true;
+					} else {
+						return false;
+					}`,
+				*/
+				{token.IF, "if"},
+				{token.LPAREN, "("},
+				{token.INT, "5"},
+				{token.LT, "<"},
+				{token.INT, "10"},
+				{token.RPAREN, ")"},
+				{token.LBRACE, "{"},
+				{token.RETURN, "return"},
+				{token.TRUE, "true"},
+				{token.SEMICOLON, ";"},
+				{token.RBRACE, "}"},
+				{token.ELSE, "else"},
+				{token.LBRACE, "{"},
+				{token.RETURN, "return"},
+				{token.FALSE, "false"},
+				{token.SEMICOLON, ";"},
+				{token.RBRACE, "}"},
 				{token.EOF, ""},
 			},
 		},
