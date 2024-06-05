cc4f8e9c6781f1b35e14ae35a0c8fa36dcfcb9fa
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-05 07:47:09 +0500

    test combined operators lexing

--- internal/lexer/lexer_test.go
+++ internal/lexer/lexer_test.go
@@ -239,13 +239,6 @@ func TestNextToken1(t *testing.T) {
 				{token.ASSIGN, "="},
 				{token.INT, "5"},
 				{token.SEMICOLON, ";"},
-				/*
-					if (5 < 10) {
-						return true;
-					} else {
-						return false;
-					}`,
-				*/
 				{token.IF, "if"},
 				{token.LPAREN, "("},
 				{token.INT, "5"},
@@ -263,6 +256,115 @@ func TestNextToken1(t *testing.T) {
 				{token.FALSE, "false"},
 				{token.SEMICOLON, ";"},
 				{token.RBRACE, "}"},
+				{token.EOF, ""},
+			},
+		},
+		{
+			name: "Lexing combined operators",
+			input: `let five = 5;
+			let ten = 10;
+			let add = fn(x, y) {
+				x + y;
+			};
+			let result = add(five, ten);
+			!-/*5;
+			5 < 10 > 5;
+			5 <= 5 >= 5;
+			if (5 < 10) {
+				return true;
+			} else {
+				return false;
+			}
+			10 == 10;
+			10 != 9;
+			`,
+			expected: []nextTokenExpectedValue{
+				{token.LET, "let"},
+				{token.ID, "five"},
+				{token.ASSIGN, "="},
+				{token.INT, "5"},
+				{token.SEMICOLON, ";"},
+
+				{token.LET, "let"},
+				{token.ID, "ten"},
+				{token.ASSIGN, "="},
+				{token.INT, "10"},
+				{token.SEMICOLON, ";"},
+
+				{token.LET, "let"},
+				{token.ID, "add"},
+				{token.ASSIGN, "="},
+				{token.FUNC, "fn"},
+				{token.LPAREN, "("},
+				{token.ID, "x"},
+				{token.COMMA, ","},
+				{token.ID, "y"},
+				{token.RPAREN, ")"},
+				{token.LBRACE, "{"},
+				{token.ID, "x"},
+				{token.PLUS, "+"},
+				{token.ID, "y"},
+				{token.SEMICOLON, ";"},
+				{token.RBRACE, "}"},
+				{token.SEMICOLON, ";"},
+
+				{token.LET, "let"},
+				{token.ID, "result"},
+				{token.ASSIGN, "="},
+				{token.ID, "add"},
+				{token.LPAREN, "("},
+				{token.ID, "five"},
+				{token.COMMA, ","},
+				{token.ID, "ten"},
+				{token.RPAREN, ")"},
+				{token.SEMICOLON, ";"},
+
+				{token.BANG, "!"},
+				{token.MINUS, "-"},
+				{token.SLASH, "/"},
+				{token.ASTERISK, "*"},
+				{token.INT, "5"},
+				{token.SEMICOLON, ";"},
+
+				{token.INT, "5"},
+				{token.LT, "<"},
+				{token.INT, "10"},
+				{token.GT, ">"},
+				{token.INT, "5"},
+				{token.SEMICOLON, ";"},
+				{token.INT, "5"},
+				{token.LT, "<"},
+				{token.ASSIGN, "="},
+				{token.INT, "5"},
+				{token.GT, ">"},
+				{token.ASSIGN, "="},
+				{token.INT, "5"},
+				{token.SEMICOLON, ";"},
+				{token.IF, "if"},
+				{token.LPAREN, "("},
+				{token.INT, "5"},
+				{token.LT, "<"},
+				{token.INT, "10"},
+				{token.RPAREN, ")"},
+				{token.LBRACE, "{"},
+				{token.RETURN, "return"},
+				{token.TRUE, "true"},
+				{token.SEMICOLON, ";"},
+				{token.RBRACE, "}"},
+				{token.ELSE, "else"},
+				{token.LBRACE, "{"},
+				{token.RETURN, "return"},
+				{token.FALSE, "false"},
+				{token.SEMICOLON, ";"},
+				{token.RBRACE, "}"},
+				{token.INT, "10"},
+				{token.EQ, "=="},
+				{token.INT, "10"},
+				{token.SEMICOLON, ";"},
+				{token.INT, "10"},
+				{token.NOT_EQ, "!="},
+				{token.INT, "9"},
+				{token.SEMICOLON, ";"},
 				{token.EOF, ""},
 			},
 		},
