82d24b398fd27400cf0d5460993ecc1cf76c863b
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-05 05:08:48 +0500

    refactoring testing

--- internal/lexer/lexer_test.go
+++ internal/lexer/lexer_test.go
@@ -6,111 +6,103 @@ import (
 	"testing"
 )
 
+type nextTokenExpectedValue struct {
+	expectedType    token.TokenType
+	expectedLiteral string
+}
+
 func TestNextToken1(t *testing.T) {
-	t.Run("Lexing initial set of tokens", func(t *testing.T) {
-		input := `=+(){},;`
+	tests := []struct {
+		name     string
+		input    string
+		expected []nextTokenExpectedValue
+	}{
+		{
+			name:  "Lexing initial set of tokens",
+			input: `=+(){},;`,
+			expected: []nextTokenExpectedValue{
+				{token.EQ, "="},
+				{token.PLUS, "+"},
+				{token.LPAREN, "("},
+				{token.RPAREN, ")"},
+				{token.LBRACE, "{"},
+				{token.RBRACE, "}"},
+				{token.COMMA, ","},
+				{token.SEMICOLON, ";"},
+			},
+		},
+		{
+			name: "Lexing numbers, identifiers and keywords",
+			input: `let five = 5;
+			let ten = 10;
 
-		tests := []struct {
-			expectedType    token.TokenType
-			expectedLiteral string
-		}{
-			{token.EQ, "="},
-			{token.PLUS, "+"},
-			{token.LPAREN, "("},
-			{token.RPAREN, ")"},
-			{token.LBRACE, "{"},
-			{token.RBRACE, "}"},
-			{token.COMMA, ","},
-			{token.SEMICOLON, ";"},
-		}
+			let add = fn(x, y) {
+				x + y;	
+			};
 
-		l := lexer.New(input)
+			let result = add(five, ten);`,
+			expected: []nextTokenExpectedValue{
+				{token.LET, "let"},
+				{token.ID, "five"},
+				{token.EQ, "="},
+				{token.INT, "5"},
+				{token.SEMICOLON, ";"},
 
-		for i, test := range tests {
-			tok := l.NextToken()
+				{token.LET, "let"},
+				{token.ID, "ten"},
+				{token.EQ, "="},
+				{token.INT, "10"},
+				{token.SEMICOLON, ";"},
 
-			if tok.Type != test.expectedType {
-				t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-					i, test.expectedType, tok.Type)
+				{token.LET, "let"},
+				{token.ID, "add"},
+				{token.EQ, "="},
+				{token.FUNC, "fn"},
+				{token.LPAREN, "("},
+				{token.ID, "x"},
+				{token.COMMA, ","},
+				{token.ID, "y"},
+				{token.RPAREN, ")"},
+				{token.LBRACE, "{"},
+				{token.ID, "x"},
+				{token.PLUS, "+"},
+				{token.ID, "y"},
+				{token.SEMICOLON, ";"},
+				{token.RBRACE, "}"},
+				{token.SEMICOLON, ";"},
+
+				{token.LET, "let"},
+				{token.ID, "result"},
+				{token.EQ, "="},
+				{token.ID, "add"},
+				{token.LPAREN, "("},
+				{token.ID, "five"},
+				{token.COMMA, ","},
+				{token.ID, "ten"},
+				{token.RPAREN, ")"},
+				{token.SEMICOLON, ";"},
+				{token.EOF, ""},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			l := lexer.New(test.input)
+
+			for i, expected := range test.expected {
+				tok := l.NextToken()
+
+				if tok.Type != expected.expectedType {
+					t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+						i, expected.expectedType, tok.Type)
+				}
+
+				if tok.Literal != expected.expectedLiteral {
+					t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+						i, expected.expectedLiteral, tok.Literal)
+				}
 			}
-
-			if tok.Literal != test.expectedLiteral {
-				t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
-					i, test.expectedLiteral, tok.Literal)
-			}
-		}
-	})
-
-	t.Run("Lexing numbers, identifiers and keywords", func(t *testing.T) {
-		input := `let five = 5;
-	let ten = 10;
-
-	let add = fn(x, y) {
-		x + y;	
-	};
-
-	let result = add(five, ten);`
-
-		tests := []struct {
-			expectedType    token.TokenType
-			expectedLiteral string
-		}{
-			{token.LET, "let"},
-			{token.ID, "five"},
-			{token.EQ, "="},
-			{token.INT, "5"},
-			{token.SEMICOLON, ";"},
-
-			{token.LET, "let"},
-			{token.ID, "ten"},
-			{token.EQ, "="},
-			{token.INT, "10"},
-			{token.SEMICOLON, ";"},
-
-			{token.LET, "let"},
-			{token.ID, "add"},
-			{token.EQ, "="},
-			{token.FUNC, "fn"},
-			{token.LPAREN, "("},
-			{token.ID, "x"},
-			{token.COMMA, ","},
-			{token.ID, "y"},
-			{token.RPAREN, ")"},
-			{token.LBRACE, "{"},
-			{token.ID, "x"},
-			{token.PLUS, "+"},
-			{token.ID, "y"},
-			{token.SEMICOLON, ";"},
-			{token.RBRACE, "}"},
-			{token.SEMICOLON, ";"},
-
-			{token.LET, "let"},
-			{token.ID, "result"},
-			{token.EQ, "="},
-			{token.ID, "add"},
-			{token.LPAREN, "("},
-			{token.ID, "five"},
-			{token.COMMA, ","},
-			{token.ID, "ten"},
-			{token.RPAREN, ")"},
-			{token.SEMICOLON, ";"},
-			{token.EOF, ""},
-		}
-
-		l := lexer.New(input)
-
-		for i, test := range tests {
-			tok := l.NextToken()
-
-			if tok.Type != test.expectedType {
-				t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-					i, test.expectedType, tok.Type)
-			}
-
-			if tok.Literal != test.expectedLiteral {
-				t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
-					i, test.expectedLiteral, tok.Literal)
-			}
-		}
-	})
+		})
+	}
 }
