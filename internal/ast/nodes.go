95482d5e51a7d75a2e8d05438200d2f9ad347eaa
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-12 11:41:19 +0500

    replace prefix expr stringify

--- internal/ast/nodes.go
+++ internal/ast/nodes.go
@@ -91,12 +91,5 @@ type PrefixExpression struct {
 func (pe *PrefixExpression) expressionNode()      {}
 func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
 func (pe *PrefixExpression) String() string {
-	var out bytes.Buffer
-
-	out.WriteString("(")
-	out.WriteString(pe.Operator)
-	out.WriteString(pe.Right.String())
-	out.WriteString(")")
-
-	return out.String()
+	return "(" + pe.Operator + pe.Right.String() + ")"
 }
