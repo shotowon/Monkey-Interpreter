b007b0962222fca469d2664d45f24f44c18e2f66
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-12 11:46:45 +0500

    stringify let statement small change

--- internal/ast/nodes.go
+++ internal/ast/nodes.go
@@ -16,9 +16,7 @@ func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 func (ls *LetStatement) String() string {
 	var out bytes.Buffer
 
-	out.WriteString(ls.TokenLiteral() + " ")
-	out.WriteString(ls.Name.String())
-	out.WriteString(" = ")
+	out.WriteString(ls.TokenLiteral() + " " + ls.Name.String() + " = ")
 
 	if ls.Value != nil {
 		out.WriteString(ls.Value.String())
