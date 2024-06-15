5051d5461ccdfd3a75ed26276144c619535c910f
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-15 21:20:19 +0500

    grouped expression tests

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -307,6 +307,26 @@ func TestStatementsParsing(t *testing.T) {
 					"3 < 4 == false",
 					"((3 < 4) == false)",
 				},
+				{
+					"1 + (2 + 3) + 4",
+					"((1 + (2 + 3)) + 4)",
+				},
+				{
+					"(5 + 5) * 2",
+					"((5 + 5) * 2)",
+				},
+				{
+					"2 / (5 + 5)",
+					"(2 / (5 + 5))",
+				},
+				{
+					"-(5 + 5)",
+					"(-(5 + 5))",
+				},
+				{
+					"!(true == true)",
+					"(!(true == true))",
+				},
 			}
 
 			for _, pTest := range precedenceTests {
