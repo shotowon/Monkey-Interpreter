da81381dac5f59513f9cb063a923b37ddfa10546
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-16 19:21:39 +0500

    token to string helper function

--- internal/token/gears.go
+++ internal/token/gears.go
@@ -17,3 +17,64 @@ func LookupID(id string) TokenType {
 
 	return ID
 }
+
+func (t TokenType) String() string {
+	switch t {
+	case INVALID:
+		return "INVALID"
+	case EOF:
+		return "EOF"
+	case ID:
+		return "ID"
+	case INT:
+		return "INT"
+	case ASSIGN:
+		return "="
+	case PLUS:
+		return "+"
+	case MINUS:
+		return "-"
+	case BANG:
+		return "!"
+	case ASTERISK:
+		return "*"
+	case SLASH:
+		return "/"
+	case EQ:
+		return "=="
+	case NOT_EQ:
+		return "!="
+	case LT:
+		return "<"
+	case GT:
+		return ">"
+	case COMMA:
+		return ","
+	case SEMICOLON:
+		return ";"
+	case LPAREN:
+		return "("
+	case RPAREN:
+		return ")"
+	case LBRACE:
+		return "{"
+	case RBRACE:
+		return "}"
+	case FUNC:
+		return "fn"
+	case LET:
+		return "let"
+	case TRUE:
+		return "true"
+	case FALSE:
+		return "false"
+	case IF:
+		return "if"
+	case ELSE:
+		return "else"
+	case RETURN:
+		return "return"
+	default:
+		return "UNKNOWN"
+	}
+}
