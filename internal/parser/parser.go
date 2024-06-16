aa3c1a780771b9728f4d78a787cde7ba408ec2a3
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-16 08:20:05 +0500

    register if expression parser-callback

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -39,6 +39,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
+	p.registerPrefix(token.IF, p.parseIfExpression)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
