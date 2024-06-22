db4d245d115605d1b9c9882505e05bf33866ee3f
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-22 12:41:28 +0500

    eval statements function

--- internal/eval/eval.go
+++ internal/eval/eval.go
@@ -5,4 +5,21 @@ import (
 	"monkey/internal/object"
 )
 
-func Eval(node ast.Node) object.Object
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+
+	return result
+}
