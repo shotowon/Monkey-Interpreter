7b7dafb60d3f1f0bf7906b2b6dfd7a305c9235b0
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-22 13:14:43 +0500

    eval prefix expression

--- internal/eval/eval.go
+++ internal/eval/eval.go
@@ -20,6 +20,15 @@ func Eval(node ast.Node) object.Object {
 	return nil
 }
 
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOpExpression(right)
+	}
+
+	return NULL
+}
+
 func evalBangOpExpression(right object.Object) object.Object {
 	switch right {
 	case TRUE:
