d765d5040fbce74f3020274e86d105a845caf2f2
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-22 12:43:22 +0500

    update eval function

--- internal/eval/eval.go
+++ internal/eval/eval.go
@@ -7,6 +7,10 @@ import (
 
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	}
