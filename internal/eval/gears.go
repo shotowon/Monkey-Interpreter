7a5b2dba092be7061dd1e15e12b0f280422bf14d
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-25 04:06:11 +0500

    eval gear function to interpret bool objects

--- internal/eval/gears.go
+++ internal/eval/gears.go
@@ -9,3 +9,14 @@ func boolToObj(value bool) *object.Boolean {
 
 	return FALSE
 }
+
+func isTrue(obj object.Object) bool {
+	switch obj {
+	case NULL, FALSE:
+		return false
+	case TRUE:
+		return true
+	default:
+		return true
+	}
+}
