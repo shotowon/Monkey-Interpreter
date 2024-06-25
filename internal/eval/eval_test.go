9f72c8855fe55d488da8587afbff40043e33a24a
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-25 14:31:15 +0500

    use env in evaluation testing

--- internal/eval/eval_test.go
+++ internal/eval/eval_test.go
@@ -209,8 +209,9 @@ func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
+	env := object.NewEnv()
 
-	return eval.Eval(program)
+	return eval.Eval(program, env)
 }
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
