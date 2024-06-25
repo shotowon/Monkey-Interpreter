57d88c1e9ac326d3767f2656d17d7d9b965836ca
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-25 13:41:02 +0500

    error handling tests

--- internal/eval/eval_test.go
+++ internal/eval/eval_test.go
@@ -122,6 +122,65 @@ func TestEval(t *testing.T) {
 			testIntegerObject(t, eval, tt.expected)
 		}
 	})
+	t.Run("test error handling", func(t *testing.T) {
+		type errTest struct {
+			input           string
+			expectedMessage string
+		}
+
+		tests := []errTest{
+			{
+				"5 + true;",
+				"type mismatch: INTEGER + BOOLEAN",
+			},
+			{
+				"5 + true; 5;",
+				"type mismatch: INTEGER + BOOLEAN",
+			},
+			{
+				"-true",
+				"unknown operator: -BOOLEAN",
+			},
+			{
+				"true + false;",
+				"unknown operator: BOOLEAN + BOOLEAN",
+			},
+			{
+				"5; true + false; 5",
+				"unknown operator: BOOLEAN + BOOLEAN",
+			},
+			{
+				"if (10 > 1) { true + false; }",
+				"unknown operator: BOOLEAN + BOOLEAN",
+			},
+			{
+				`
+				132
+				if (10 > 1) {
+					if (10 > 1) {
+						return true + false;
+					}
+					return 1;
+				}
+				`,
+				"unknown operator: BOOLEAN + BOOLEAN",
+			},
+		}
+
+		for _, tt := range tests {
+			eval := testEval(tt.input)
+
+			errObj, ok := eval.(*object.Error)
+			if !ok {
+				t.Errorf("no error object returned. got=%T(%+v)", eval, eval)
+				continue
+			}
+
+			if errObj.Message != tt.expectedMessage {
+				t.Errorf("wrong error message. got=%q expected=%q", errObj.Message, tt.expectedMessage)
+			}
+		}
+	})
 }
 
 func testEval(input string) object.Object {
