b6bc1e867571cb346315dcf71b6e382ab4888b35
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-27 00:51:54 +0500

    string literal evaluation

--- internal/eval/eval.go
+++ internal/eval/eval.go
@@ -51,6 +51,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return applyFunc(function, args)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
 	case *ast.BlockStatement:
 		return evalBlockStmt(node, env)
 	case *ast.IfExpression:
