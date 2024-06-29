ea130c6a236c8bca3db6780be8e60acda3cca6de
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-29 20:26:20 +0500

    array literal AST node

--- internal/ast/nodes.go
+++ internal/ast/nodes.go
@@ -218,3 +218,22 @@ type StringLiteral struct {
 func (sl *StringLiteral) expressionNode()      {}
 func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
 func (sl *StringLiteral) String() string       { return sl.Token.Literal }
+
+type ArrayLiteral struct {
+	Token    token.Token
+	Elements []Expression
+}
+
+func (al *ArrayLiteral) expressionNode()      {}
+func (al *ArrayLiteral) TokenLiteral() string { return al.Token.Literal }
+func (al *ArrayLiteral) String() string {
+	var out bytes.Buffer
+
+	elements := make([]string, len(al.Elements))
+	for i, e := range al.Elements {
+		elements[i] = e.String()
+	}
+
+	out.WriteString("[" + strings.Join(elements, ", ") + "]")
+	return out.String()
+}
