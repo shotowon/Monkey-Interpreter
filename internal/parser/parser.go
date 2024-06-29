52ed485141ed876830e7531097b57cb8ccc3b403
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-29 20:41:30 +0500

    array  literal parsing

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -43,6 +43,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.IF, p.parseIfExpression)
 	p.registerPrefix(token.FUNC, p.parseFunctionLiteral)
 	p.registerPrefix(token.STRING, p.parseStringLiteral)
+	p.registerPrefix(token.LBRACKET, p.parseArrayLiteral)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
